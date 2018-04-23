77cdc5873d7087dc9d783fcab8936f48f2e729f6
Greg Sherrid <gregsherrid@gmail.com>
2018-04-23 14:16:16 -0700

    update updater

--- updater.rb
+++ updater.rb
@@ -15,6 +15,6 @@ end
 BASE = "https://raw.githubusercontent.com/gregsherrid/batchmailer/master"
 
 # I hope its ok to update the source code of a file that's currently running...
-FILES_TO_UPDATE = ["README.md", "main.rb", "updater.rb"]
+FILES_TO_UPDATE = ["README.md", "main.rb", "updater.rb", "Gemfile", "Gemfile.lock"]
 
 main
